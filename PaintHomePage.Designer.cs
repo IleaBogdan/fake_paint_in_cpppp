0728c98f3ee1eed74293703a9ecdbed160654d84
Batman <139052441+IleaBogdan@users.noreply.github.com>
2026-05-26 16:03:18 +0300

    project

---
+++ PaintHomePage.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace proiect_atestat_random_nou_pt_nota_10
+{
+    partial class PaintHomePage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.clear_button = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.draw_preset_button = new System.Windows.Forms.Button();
+            this.undo_button = new System.Windows.Forms.Button();
+            this.buttonPickColor = new System.Windows.Forms.Button();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(11, 228);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(882, 411);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(692, 11);
+            this.listView1.Margin = new System.Windows.Forms.Padding(2);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(201, 178);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(98, 12);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(85, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Mouse Width:";
+            // 
+            // clear_button
+            // 
+            this.clear_button.Location = new System.Drawing.Point(13, 164);
+            this.clear_button.Name = "clear_button";
+            this.clear_button.Size = new System.Drawing.Size(79, 25);
+            this.clear_button.TabIndex = 4;
+            this.clear_button.Text = "Clear";
+            this.clear_button.UseVisualStyleBackColor = true;
+            this.clear_button.Click += new System.EventHandler(this.clear_button_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(536, 14);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(125, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(432, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Predefined shapes:";
+            // 
+            // draw_preset_button
+            // 
+            this.draw_preset_button.Location = new System.Drawing.Point(567, 41);
+            this.draw_preset_button.Name = "draw_preset_button";
+            this.draw_preset_button.Size = new System.Drawing.Size(94, 37);
+            this.draw_preset_button.TabIndex = 7;
+            this.draw_preset_button.Text = "Draw Shape";
+            this.draw_preset_button.UseVisualStyleBackColor = true;
+            this.draw_preset_button.Click += new System.EventHandler(this.draw_preset_button_Click);
+            // 
+            // undo_button
+            // 
+            this.undo_button.Location = new System.Drawing.Point(108, 164);
+            this.undo_button.Name = "undo_button";
+            this.undo_button.Size = new System.Drawing.Size(75, 25);
+            this.undo_button.TabIndex = 8;
+            this.undo_button.Text = "Undo";
+            this.undo_button.UseVisualStyleBackColor = true;
+            this.undo_button.Click += new System.EventHandler(this.undo_button_Click);
+            // 
+            // buttonPickColor
+            // 
+            this.buttonPickColor.Location = new System.Drawing.Point(557, 132);
+            this.buttonPickColor.Name = "buttonPickColor";
+            this.buttonPickColor.Size = new System.Drawing.Size(104, 57);
+            this.buttonPickColor.TabIndex = 9;
+            this.buttonPickColor.Text = "Pick Color";
+            this.buttonPickColor.UseVisualStyleBackColor = true;
+            this.buttonPickColor.Click += new System.EventHandler(this.buttonPickColor_Click);
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(195, 14);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 10;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 79);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(170, 36);
+            this.progressBar1.TabIndex = 11;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(434, 67);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(117, 109);
+            this.checkedListBox1.TabIndex = 12;
+            // 
+            // PaintHomePage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 650);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.buttonPickColor);
+            this.Controls.Add(this.undo_button);
+            this.Controls.Add(this.draw_preset_button);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.clear_button);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "PaintHomePage";
+            this.Text = "Paint";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button clear_button;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button draw_preset_button;
+        private System.Windows.Forms.Button undo_button;
+        private System.Windows.Forms.Button buttonPickColor;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+    }
+}
+
---
+++ PaintHomePage.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace proiect_atestat_random_nou_pt_nota_10
+{
+    public partial class PaintHomePage : Form
+    {
+        List<string>shape_names=new List<string>();
+        List<List<Point>> shapes = new List<List<Point>>();
+        Dictionary<string, bool> shapeHiddenStatus = new Dictionary<string, bool>();
+
+        public PaintHomePage()
+        {
+            InitializeComponent();
+            this.init_shapes();
+            this.clear_picturebox();
+            listView1.View = View.Details;
+            listView1.Columns.Add("Shapes", 100);
+            listView1.Columns.Add("Times Drawn", 100);
+
+            foreach (string shape in shape_names)
+            {
+                this.listView1.Items.Add(shape).SubItems.Add("0");
+                this.comboBox1.Items.Add(shape);
+                this.checkedListBox1.Items.Add(shape, true);
+                this.shapeHiddenStatus[shape] = false; 
+            }
+            this.checkedListBox1.ItemCheck += checkedListBox1_ItemCheck;
+
+            this.progressBar1.Maximum = 100;
+            this.progressBar1.Value = 100;
+            this.UpdateProgressBar();
+        }
+        private bool drawing = false;
+        private Point lastPoint;
+        private void checkedListBox1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            string shapeName = this.checkedListBox1.Items[e.Index].ToString();
+
+            // Update hidden status based on new check state
+            bool willBeHidden = (e.NewValue == CheckState.Unchecked);
+            this.shapeHiddenStatus[shapeName] = willBeHidden;
+
+            // Refresh the listView to show/hide items
+            this.RefreshListView();
+        }
+
+        private void RefreshListView()
+        {
+            Dictionary<string, string> shapeCounts = new Dictionary<string, string>();
+            foreach (ListViewItem item in this.listView1.Items)
+            {
+                shapeCounts[item.Text] = item.SubItems[1].Text;
+            }
+            this.listView1.Items.Clear();
+            for (int i = 0; i < this.shape_names.Count; i++)
+            {
+                string shape = this.shape_names[i];
+                if (!this.shapeHiddenStatus[shape])
+                {
+                    ListViewItem newItem = this.listView1.Items.Add(shape);
+                    newItem.SubItems.Add(shapeCounts.ContainsKey(shape) ? shapeCounts[shape] : "0");
+                }
+            }
+        }
+
+        private void clear_picturebox()
+        {
+            // init picturebox
+            if (pictureBox1.Image != null)
+            {
+                pictureBox1.Image.Dispose();
+            }
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clear(Color.White);
+            }
+            pictureBox1.Image = bmp;
+            if (this.pictureBox1.Image != null)
+            {
+                undoStack.Push(new Bitmap(this.pictureBox1.Image));
+            }
+            this.UpdateProgressBar();
+        }
+
+        private void save_checkpoint()
+        {
+            if (this.pictureBox1.Image != null)
+            {
+                undoStack.Push(new Bitmap(this.pictureBox1.Image));
+            }
+        }
+
+        private Stack<Bitmap> undoStack = new Stack<Bitmap>();
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.drawing = true;
+            this.lastPoint = e.Location;
+            this.save_checkpoint();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (this.drawing)
+            {
+                using (Graphics g = Graphics.FromImage(this.pictureBox1.Image))
+                {
+                    g.DrawLine(new Pen(this.mouse_color, this.mouse_width), this.lastPoint, e.Location);
+                }
+                this.lastPoint = e.Location;
+                this.pictureBox1.Refresh();
+            }
+        }
+
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.drawing = false;
+            this.UpdateProgressBar();
+        }
+
+        private Color mouse_color = Color.Black;
+        private int mouse_width = 5;
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            this.mouse_width=(int)numericUpDown1.Value;
+        }
+
+        private void clear_button_Click(object sender, EventArgs e)
+        {
+            this.clear_picturebox();
+        }
+        private void draw(int idx)
+        {
+            //this.save_checkpoint();
+            using (Graphics g = this.pictureBox1.CreateGraphics())
+            {
+                for (int i = 0; i < this.shapes[idx].Count; ++i)
+                {
+                    g.DrawLine(new Pen(this.mouse_color, this.mouse_width), this.shapes[idx][i], this.shapes[idx][(i+1)% this.shapes[idx].Count]);
+                }
+            }
+            this.UpdateProgressBar();
+        }
+
+        private void draw_preset_button_Click(object sender, EventArgs e)
+        {
+            int idx = this.comboBox1.SelectedIndex;
+            if (idx < 0) return;
+
+            string selectedShape = this.shape_names[idx];
+
+            if (!this.shapeHiddenStatus[selectedShape])
+            {
+                foreach (ListViewItem item in this.listView1.Items)
+                {
+                    if (item.Text == selectedShape)
+                    {
+                        item.SubItems[1].Text = (Int32.Parse(item.SubItems[1].Text) + 1).ToString();
+                        break;
+                    }
+                }
+            }
+
+            this.draw(idx);
+        }
+
+        private void undo_button_Click(object sender, EventArgs e)
+        {
+            if (undoStack.Count > 0)
+            {
+                var prevstate=this.undoStack.Pop();
+                if (pictureBox1.Image != null)
+                {
+                    pictureBox1.Image.Dispose();
+                }
+                pictureBox1.Image = prevstate;
+                pictureBox1.Refresh();
+            }
+            this.UpdateProgressBar();
+        }
+
+        private void buttonPickColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                this.mouse_color = colorDialog.Color;
+            }
+        }
+        private void UpdateProgressBar()
+        {
+            if (pictureBox1.Image != null)
+            {
+                Bitmap bmp = (Bitmap)pictureBox1.Image;
+                int totalPixels = bmp.Width * bmp.Height;
+                int nonWhitePixels = 0;
+
+                // Count non-white pixels (drawn pixels)
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    for (int x = 0; x < bmp.Width; x++)
+                    {
+                        Color pixel = bmp.GetPixel(x, y);
+                        if (pixel.R != 255 || pixel.G != 255 || pixel.B != 255)
+                        {
+                            nonWhitePixels++;
+                        }
+                    }
+                }
+
+                int usedPercentage = (int)(((double)nonWhitePixels / totalPixels) * 100);
+                progressBar1.Value = Math.Min(100, usedPercentage);
+            }
+        }
+
+        private void init_shapes()
+        {
+            // square
+            shape_names.Add("Squares");
+            {
+                var square = new List<Point>();
+                square.Add(new Point(pictureBox1.Size.Width / 2 - 50, pictureBox1.Size.Height / 2 - 50));
+                square.Add(new Point(pictureBox1.Size.Width / 2 + 50, pictureBox1.Size.Height / 2 - 50));
+                square.Add(new Point(pictureBox1.Size.Width / 2 + 50, pictureBox1.Size.Height / 2 + 50));
+                square.Add(new Point(pictureBox1.Size.Width / 2 - 50, pictureBox1.Size.Height / 2 + 50));
+                shapes.Add(square);
+            }
+
+            // triangle
+            shape_names.Add("Triangles");
+            {
+                var triangle = new List<Point>();
+                triangle.Add(new Point(pictureBox1.Size.Width / 2, pictureBox1.Size.Height / 2 - 50));
+                triangle.Add(new Point(pictureBox1.Size.Width / 2 + 50, pictureBox1.Size.Height / 2 + 50));
+                triangle.Add(new Point(pictureBox1.Size.Width / 2 - 50, pictureBox1.Size.Height / 2 + 50));
+                shapes.Add(triangle);
+            }
+
+            // rectangle
+            shape_names.Add("Rectangles");
+            {
+                var rectangle = new List<Point>();
+                rectangle.Add(new Point(pictureBox1.Size.Width / 2 - 75, pictureBox1.Size.Height / 2 - 40));
+                rectangle.Add(new Point(pictureBox1.Size.Width / 2 + 75, pictureBox1.Size.Height / 2 - 40));
+                rectangle.Add(new Point(pictureBox1.Size.Width / 2 + 75, pictureBox1.Size.Height / 2 + 40));
+                rectangle.Add(new Point(pictureBox1.Size.Width / 2 - 75, pictureBox1.Size.Height / 2 + 40));
+                shapes.Add(rectangle);
+            }
+
+            // pentagon
+            shape_names.Add("Pentagons");
+            {
+                var pentagon = new List<Point>();
+                pentagon.Add(new Point(pictureBox1.Size.Width / 2, pictureBox1.Size.Height / 2 - 50));
+                pentagon.Add(new Point(pictureBox1.Size.Width / 2 + 47, pictureBox1.Size.Height / 2 - 15));
+                pentagon.Add(new Point(pictureBox1.Size.Width / 2 + 29, pictureBox1.Size.Height / 2 + 40));
+                pentagon.Add(new Point(pictureBox1.Size.Width / 2 - 29, pictureBox1.Size.Height / 2 + 40));
+                pentagon.Add(new Point(pictureBox1.Size.Width / 2 - 47, pictureBox1.Size.Height / 2 - 15));
+                shapes.Add(pentagon);
+            }
+        }
+    }
+}
